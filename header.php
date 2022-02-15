27c71d93367aa96749fe4aeb1714fe8402ef9fc6
unknown <fauzan.alghifari17095@student.unsika.ac.id>
2022-02-15 08:49:16 +0700

    Wordpress hari ke lima

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php bloginfo('name'); ?></title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header>
+		<h1><a href="<?php echo home_url(); ?>"> <?php bloginfo('name'); ?></a></h1>
+		<h2><?php bloginfo('description'); ?></h2>
+		<nav>
+		<?php 
+			$args = array ('theme_location' => 'main_menu');
+			wp_nav_menu($args); 
+		?>
+		</nav>
+
+        <div id="search_form">
+         <?php get_search_form(); ?>
+        </div>
+        <div class="clear"></div>
+	</header>
+
+</body>
+</html>
